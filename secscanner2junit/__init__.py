433562945c219cf54013464320163b8178e7db00
Florian Angermeir <florian.angermeir@tum.de>
2022-02-07 14:40:51 +0100

    Restructure calling sequences

--- secscanner2junit/__init__.py
+++ secscanner2junit/__init__.py
@@ -2,11 +2,13 @@ import sys
 import enum
 import json
 from junit_xml import to_xml_report_file
-from .gitleaks import GitleaksParser
+from .secrets import SecretsParser
+from .sast import SastParser
 
 
 class ScanType(enum.Enum):
-    GITLEAKS = 'gitleaks'
+    SECRETS = 'secrets'
+    SAST = 'sast'
 
 
 def load(input_path):
@@ -17,14 +19,16 @@ def load(input_path):
 
 def save_junit_report(testsuite, output_path):
     with open(output_path, 'w') as output_file:
-        to_xml_report_file(output_file, [testsuite], prettyprint=False)
+        to_xml_report_file(output_file, testsuite, prettyprint=True)
 
 
 def main():
     scan_type, input_path, output_path = sys.argv[1], sys.argv[2], sys.argv[3]
     report = load(input_path)
-    if scan_type == ScanType.GITLEAKS.value:
-        parser = GitleaksParser(report, input_path)
+    if scan_type == ScanType.SECRETS.value:
+        parser = SecretsParser(report, input_path)
+    elif scan_type == ScanType.SAST.value:
+        parser = SastParser(report, input_path)
     else:
         raise NotImplementedError
     testsuite = parser.parse()
