ecee277f8825c986074f137052b9dfd26444c69e
Florian Angermeir <florian.angermeir@tum.de>
2023-01-03 12:15:18 +0100

    28 more user friendly cli argument parser (#31)
    * Parse arguments with argparse

--- secscanner2junit/__init__.py
+++ secscanner2junit/__init__.py
@@ -1,6 +1,8 @@
 import enum
 import json
 import sys
+import argparse
+import pkg_resources
 
 from junit_xml import to_xml_report_file
 
@@ -15,6 +17,10 @@ class ScanType(enum.Enum):
     SAST = 'sast'
     CS = 'container_scanning'
 
+    @staticmethod
+    def list():
+        return list(map(lambda x: x.value, ScanType))
+
 
 def load_report(input_path):
     with open(input_path) as input_file:
@@ -27,27 +33,35 @@ def save_junit_report(testsuite, output_path):
         to_xml_report_file(output_file, testsuite, prettyprint=True)
 
 
-def load_config():
-    if len(sys.argv) >= 5:
-        config_path = sys.argv[4]
-        return get_config(config_path)
-    return Config([])
+def parse_arguments(args):
+    arg_parser = argparse.ArgumentParser(description="SecScanner2JUnit: Convert security scanner output to JUnit format.")
+    arg_parser.add_argument('--version', action='version', version=pkg_resources.get_distribution('secscanner2junit').version)
+    arg_parser.add_argument('activity', choices=ScanType.list())
+    arg_parser.add_argument('input_file')
+    arg_parser.add_argument('output_file')
+    arg_parser.add_argument('config', nargs='?')
+    return arg_parser.parse_args(args)
 
 
-def main():
-    scan_type, input_path, output_path = sys.argv[1], sys.argv[2], sys.argv[3]
-    ss2ju_config = load_config()
-    report = load_report(input_path)
-    if scan_type == ScanType.SECRETS.value:
-        parser = SecretsParser(report, input_path, ss2ju_config)
-    elif scan_type == ScanType.SAST.value:
-        parser = SastParser(report, input_path, ss2ju_config)
-    elif scan_type == ScanType.CS.value:
-        parser = ContainerScanningParser(report, input_path, ss2ju_config)
+def main(args=None):
+    if args is None:
+        args = parse_arguments(sys.argv[1:])
+    if args.config:
+        ss2ju_config = get_config()
+    else:
+        ss2ju_config = Config([])
+
+    report = load_report(args.input_file)
+    if args.activity == ScanType.SECRETS.value:
+        parser = SecretsParser(report, args.input_file, ss2ju_config)
+    elif args.activity == ScanType.SAST.value:
+        parser = SastParser(report, args.input_file, ss2ju_config)
+    elif args.activity == ScanType.CS.value:
+        parser = ContainerScanningParser(report, args.input_file, ss2ju_config)
     else:
         raise NotImplementedError
     testsuite = parser.parse()
-    save_junit_report(testsuite, output_path)
+    save_junit_report(testsuite, args.output_file)
 
 
 if __name__ == '__main__':
